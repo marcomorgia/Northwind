f5bd85fbe6fd98d309c84402b67bed931f4e9371
Marco Morgia <marco.morgia83@gmail.com>
2017-06-21 22:06:25 +0200

    First Commit

---
+++ Northwind.ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Northwind.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        private IList<Customers> _customers;
+
+        public IList<Customers> Customers
+        {
+          get
+            {
+                if (_customers == null)
+                    GetCustomers();
+
+                return _customers;
+            }
+        }
+
+        private void GetCustomers()
+        {
+            _customers = new NORTHWNDEntities().Customers.ToList();
+        }
+    }
+}
---
+++ Northwind.ViewModel/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.ViewModel
+{
+    public class ViewModelLocator
+    {
+        private static MainWindowViewModel _mainWindowViewModel;
+        public static MainWindowViewModel MainWindowViewModelStatic
+        {
+            get
+            {
+                if (_mainWindowViewModel == null)
+                    _mainWindowViewModel = new MainWindowViewModel();
+
+                return _mainWindowViewModel;
+            }
+        }
+    }
+}
