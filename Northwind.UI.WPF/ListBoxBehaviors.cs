a66fdf2d6e5d360ce519eed3e3352fb517608e04
Marco Morgia <marco.morgia83@gmail.com>
2017-06-27 09:07:59 +0200

    Cpt5

---
+++ Northwind.UI.WPF/ListBoxBehaviors.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Northwind.UI.WPF
+{
+    static class ListBoxBehaviors
+    {
+
+
+        public static void SetDoubleClickCommand(UIElement element, ICommand value)
+        {
+            element.SetValue(DoubleClickCommandProperty, value);
+        }
+
+        public static ICommand GetDoubleClickCommand(UIElement element)
+        {
+            return (ICommand)element.GetValue(DoubleClickCommandProperty);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DoubleClickCommandProperty =
+            DependencyProperty.RegisterAttached("DoubleClickCommand", typeof(ICommand), typeof(ListBoxBehaviors), new PropertyMetadata(null, new PropertyChangedCallback(DoubleClickCommand_PropertyChanged)));
+
+        private static void DoubleClickCommand_PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UIElement target = d as UIElement;
+
+            if (e.OldValue != null)
+            {
+                target.RemoveHandler(ListBox.MouseDoubleClickEvent, new RoutedEventHandler(ListBox_DoubleClick));
+            }
+            if (e.NewValue != null)
+            {
+                target.AddHandler(ListBox.MouseDoubleClickEvent, new RoutedEventHandler(ListBox_DoubleClick));
+            }
+        }
+
+        private static void ListBox_DoubleClick(object sender, RoutedEventArgs e)
+        {
+            ListBox listBox = sender as ListBox;
+            ICommand doubleClickCommand = GetDoubleClickCommand(listBox);
+
+            if (doubleClickCommand.CanExecute(e))
+            {
+                doubleClickCommand.Execute(e);
+            }
+        }
+    }
+}
